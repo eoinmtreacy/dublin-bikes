659c021565f15d2b68a722aee536e5fa8ea4d901
Ryan Davey <104937477+RyanDaveyIE@users.noreply.github.com>
2024-02-18 19:22:25 +0000

    Update constants.py
    Updated constants.py to include DB_PORT and WEATHER_API_KEY

--- prototypes/prototypes_scraper/constants.py
+++ prototypes/prototypes_scraper/constants.py
@@ -2,6 +2,9 @@ DB = 'dublin-bikes-rds.c7w22mqua0gp.eu-north-1.rds.amazonaws.com'
 DB_USER = 'admin'
 DB_PW = 'godilovenetworks'
 DB_NAME = 'dbikes'
+DB_PORT = '3306'
 
 JCD_API_KEY = '4c0892699aba5ab6f5f9c65bfb93fcd6fae2e49f'
 CITY = 'dublin'
+
+WEATHER_API_KEY = 'KQr7EcPoIC7ZmAHwkLI57Tr3KIA31a9m'
