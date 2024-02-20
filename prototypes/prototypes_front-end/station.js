23db262ec3e60d85412920a9acf90dad54b220a3
eoin m treacy <eointreacy5@gmail.com>
2024-02-20 15:50:54 +0000

    station.js object prototypes

--- prototypes/prototypes_front-end/station.js
+++ prototypes/prototypes_front-end/station.js
@@ -1,5 +1,4 @@
 class Station {
-
     constructor(id, lat, long, free, parking) {
         this.id = id
         this.lat = lat
@@ -36,9 +35,9 @@ class Station {
         this.free = vale
     }
   }
-  
-  class Cluster {
 
+
+class Cluster {
     constructor(stations) {
         // constructor accept an array of Station objects
         this.stations = stations
@@ -49,18 +48,27 @@ class Station {
     }
 
     getFree() {
-        const sum = this.stations.reduce((acc, station) => acc + station.getFree(), 0);
-        return sum;
+        return this.stations.reduce((acc, station) => acc + station.getFree(), 0)
     }
-  }
+
+    getParking() {
+        return this.stations.reduce((acc, station) => acc + station.getParking(), 0)
+    }
+}
+
+// if you run this file you'll see how a Station and a Cluster are constructed and 
 
 const stations = [
-    new Station(1,2,3,4,5),
-    new Station(1,2,3,4,5),
-    new Station(1,2,3,4,5),
-    new Station(1,2,3,4,5),
-    new Station(1,2,3,4,5),
+    new Station(1, 2, 3, 4, 5),
+    new Station(420, 2, 3, 4, 5),
+    new Station(69, 2, 3, 4, 5),
+    new Station(123, 2, 3, 4, 5),
+    new Station(0, 2, 3, 4, 5),
 ]
 
 const cluster = new Cluster(stations)
-console.log(cluster.getFree())
+console.log(cluster.getStations());
+console.log("Free spaces in cluster are: " + cluster.getFree());
+console.log("Free parking spaces in cluster are: " + cluster.getParking());
+
+stations.forEach((station) => console.log(station, "Free parking: ", station.getParking()))
