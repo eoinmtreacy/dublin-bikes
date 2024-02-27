c88a53d564b6a115221d6fac2301e11aa340cb71
eoin m treacy <eointreacy5@gmail.com>
2024-02-27 09:13:02 +0000

    add dublin.json

--- prototypes/prototypes_back-end/app.py
+++ prototypes/prototypes_back-end/app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
+import json
 
 # initialise flask app
 app = Flask(__name__)
+
+with open('./prototypes/prototypes_back-end/dublin.json', 'r') as file:
+    # Load JSON data from the file
+    data = json.load(file)
+
+print([(station['number'], station['name'], station['latitude'], station['longitude']) for station in data])
 
 # create landing page
 @app.route('/')
 def landing():
     """serve landing page html from /templates folder"""
     return render_template('draft4.html')
+
+# Open the JSON file for reading
 
 if __name__ == '__main__':
     app.run(debug=True)
