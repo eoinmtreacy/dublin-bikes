b68e59b63e1782c68b4b211de172efb9fc77c159
eoin m treacy <eointreacy5@gmail.com>
2024-02-27 09:37:27 +0000

    add weather api call

--- prototypes/prototypes_back-end/app.py
+++ prototypes/prototypes_back-end/app.py
@@ -1,19 +1,33 @@
 from flask import Flask, render_template
 import json
+from constants import *
+import requests
 
 # initialise flask app
 app = Flask(__name__)
 
-with open('./prototypes/prototypes_back-end/dublin.json', 'r') as file:
-    # Load JSON data from the file
-    data = json.load(file)
+# with open('./prototypes/prototypes_back-end/dublin.json', 'r') as file:
+#     # Load JSON data from the file
+#     data = json.load(file)
 
-print([(station['number'], station['name'], station['latitude'], station['longitude']) for station in data])
+# print([(station['number'], station['name'], station['latitude'], station['longitude']) for station in data])
+
+
 
 # create landing page
 @app.route('/')
 def landing():
-    """serve landing page html from /templates folder"""
+    """serve landing page html from /templates folder
+    calls weather api
+    """
+
+    headers = {"accept": "application/json"}
+    url = f"https://api.tomorrow.io/v4/weather/realtime?location={CITY}&units=metric&apikey={WEATHER_API_KEY}"
+
+    response = requests.get(url, headers=headers)
+
+    print(response.status_code)
+
     return render_template('draft4.html')
 
 # Open the JSON file for reading
