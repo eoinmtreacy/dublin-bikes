9ffe97c1dee0301cf1bcd015de44e8d0a700bb2d
Ryan Davey <104937477+RyanDaveyIE@users.noreply.github.com>
2024-02-28 21:08:45 +0000

    Update constants.py

--- prototypes/prototypes_scraper/constants.py
+++ prototypes/prototypes_scraper/constants.py
@@ -2,6 +2,9 @@ DB = 'dublin-bikes-rds.c7w22mqua0gp.eu-north-1.rds.amazonaws.com'
 DB_USER = 'admin'
 DB_PW = 'godilovenetworks'
 DB_NAME = 'dbikes'
+DB_PORT = '3306'
+
+DB_URI = f'mysql+pymysql://{DB_USER}:{DB_PW}@{DB}:{DB_PORT}/{DB_NAME}'
 
 JCD_API_KEY = '4c0892699aba5ab6f5f9c65bfb93fcd6fae2e49f'
 CITY = 'dublin'
@@ -9,3 +12,7 @@ CITY = 'dublin'
 EC2 = 'ec2-13-48-203-122.eu-north-1.compute.amazonaws.com'
 
 WEATHER_API_KEY = 'KQr7EcPoIC7ZmAHwkLI57Tr3KIA31a9m'
+
+FLASK_SECRET_KEY = 'key'
+
+
