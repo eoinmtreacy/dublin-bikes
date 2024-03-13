a5f2cdcb87653ede49ca45f63589b9c82b3f16b7
Ryan Davey <104937477+RyanDaveyIE@users.noreply.github.com>
2024-03-13 23:43:57 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,11 +2,17 @@ DB = 'dublin-bikes-rds.c7w22mqua0gp.eu-north-1.rds.amazonaws.com'
 DB_USER = 'admin'
 DB_PW = 'godilovenetworks'
 DB_NAME = 'dbikes'
+DB_PORT = '3306'
 
 JCD_API_KEY = '4c0892699aba5ab6f5f9c65bfb93fcd6fae2e49f'
 CITY = 'dublin'
 
 EC2 = 'ec2-13-48-203-122.eu-north-1.compute.amazonaws.com'
 
-WEATHER_API_KEY = 'KQr7EcPoIC7ZmAHwkLI57Tr3KIA31a9m'
+SECRET_KEY = 'key'
+
+WEATHER_API_KEY = '2958a388cbe248ef997234058240603&q'
 GOOGLE_MAPS_API_KEY = 'AIzaSyBbyqcHXdStChD06jciUCL-41mCkCv-uUQ'
+
+
+DB_URI = f'mysql+pymysql://{DB_USER}:{DB_PW}@{DB}:{DB_PORT}/{DB_NAME}'
