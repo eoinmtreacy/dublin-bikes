c9d43b7c4f78a6d4003f81905d9479e7f8f298b0
eoin m treacy <eointreacy5@gmail.com>
2024-03-14 14:23:52 +0000

    create_db_station_json

---
+++ create_db.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from constants import *
+import sys
+
+def fetch_city_static(arg):
+        try:
+            r = requests.get(f'https://api.jcdecaux.com/vls/v1/stations?contract={arg}&apiKey={JCD_API_KEY}')
+            # parse json
+            data = json.loads(r.text)
+            print(data)
+
+            with open(f"./stations/{arg}_stations.json", "w") as json_file:
+                json_file.write(json.dumps(data, indent=4))
+        except:
+             "Error with request to JCDecaux API: check your contract name and API key"
+
+        
+
+if __name__ == "__main__":
+    # Check if there is exactly one command-line argument (excluding the script name)
+    if len(sys.argv) != 2:
+        print("Usage: python static_create.py <city_name>")
+        sys.exit(1)
+
+    # Extract the argument
+    arg = sys.argv[1]
+
+    # Process the argument
+    fetch_city_static(arg)
+
+
