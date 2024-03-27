e69e3e9061c67b01b9eb3ebf0de7e76f3ab7b31a
eoin m treacy <eointreacy5@gmail.com>
2024-03-27 17:17:21 +0000

    change connector string

--- realtime_test.py
+++ realtime_test.py
@@ -9,28 +9,36 @@ def main():
     return for pop-up UI"""
 
     try:
+        # conn = mysql.connector.connect(
+        # host=DB,
+        # user=DB_USER,
+        # password=DB_PW,
+        # database=CITY
+        # )
+
         conn = mysql.connector.connect(
         host=DB,
         user=DB_USER,
-        password=DB_PW,
-        database=CITY
-        )
+        password=DB_PW
+    )
 
-        cursor = conn.cursor()
+        # cursor = conn.cursor()
 
-        query = (
-            """SELECT number, available_bikes, MAX(last_update) AS time
-            FROM availability
-            GROUP BY number;
-            """
-        )
+        # query = (
+        #     """SELECT number, available_bikes, MAX(last_update) AS time
+        #     FROM availability
+        #     GROUP BY number;
+        #     """
+        # )
 
-        cursor.execute(query)
-        results = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        print(jsonify(results))
+        # cursor.execute(query)
+        # results = cursor.fetchall()
+        # cursor.close()
+        # conn.close()
+        # print(jsonify(results))
         # return jsonify(results)
+
+        print("SUCCESS")
 
     except:
         print('FAILURE realtime')
